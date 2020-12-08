2a224bd2555f15c409365610b573b746c975b633
Laura <laurabande10@gmail.com>
2020-12-08 17:11:59 +0100

    Creación de objetos e impresión de valores

--- src/main/java/cris/tarea4cristinaj/Prueba.java
+++ src/main/java/cris/tarea4cristinaj/Prueba.java
@@ -3,7 +3,11 @@ package cris.tarea4cristinaj;
 public class Prueba {
 
 	public static void main(String[] args) {
-
+		Galletas g = new Galletas();
+		Galletas choco = new Galletas("chocolate", 3, 5.5);
+		
+		System.out.println(choco.toString());
+		System.out.println(g.toString());
 	}
 
 }
