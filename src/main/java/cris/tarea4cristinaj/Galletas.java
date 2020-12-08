86d704c727cd54738aec9553741b62e23b6681bc
cristinaj1 <cristinacee305@gmail.com>
2020-12-08 16:56:50 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/cris/tarea4cristinaj/Galletas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cris.tarea4cristinaj;
+
+/**
+ *
+ * @author Cris
+ */
+public class Galletas {
+    public String sabor;
+    public int radio;
+    public double precio;
+    
+    
+}
